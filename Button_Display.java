e905aa1d2d846b3eb6cf643832bf9f54d7ecb904
Shikhamishra12 <74186594+Shikhamishra12@users.noreply.github.com>
2020-11-10 13:33:59 +0530

    Create Button_Display.java

---
+++ Button_Display.java
@@ -0,0 +1,23 @@
+String p=(String)l1.getSelectedValue();
+if(p.equals(“PRITHVI”)) {
+ta.setText(“”);
+ta.append(“The Government of India launched the Integrated Guided Missile Development. Variants make use of either liquid or both liquid and solid fuels. Developed as a battlefield missile, it could carry a nuclear warhead in its role as a tactical nuclear weapon.”);
+L2.setIcon(new ImageIcon(“E:\\ip\\weapons pic\\New folder\\capture.PNG”));
+ }
+if(p.equals(“NIRBHAY”)) 
+       {
+ta.setText(“”);
+ta.append(“Nirbhay is able to pick out a target and attack it among multiple targets. The missile has a loitering capability, i.e., it can go round a target and perform several manoeuvres and then re-engage it.”);
+L2.setIcon(new ImageIcon(“E:\\ip\\weapons pic\\New folder\\jas.JPG”));}
+if(p.equals(“AKASH”)) {
+ta.setText(“”);
+ta.append(“The missile system can target aircraft up to 30 km away, at altitudes up to 18,000 m. It has the capability aerial targets like fighter jets, cruise missiles and air-to-surface missiles\” as well as It is in operational service with the Indian Army and the Indian Air Force.”);
+L2.setIcon(new ImageIcon(“E:\\ip\\weapons pic\\New folder\\akash.JPG”));}
+if(p.equals(“SHAURYA”)) {
+ta.setText(“”);
+ta.append(“The  missile is speculated to be the land version of the under-water  K-15 missile, although DRDO officials have reportedly denied its connection with the K-15  is stored in a composite canister, which makes it much easier to store for long periods without maintenance as well as to handle and transport. “);
+L2.setIcon(new ImageIcon(“E:\\ip\\weapons pic\\New folder\\SHA.JPG”));}
+if(p.equals(“DHANUSH”)) {
+ta.setText(“”);
+ta.append(“Dhanush is a variant of the surface-to-surface/ship-to-ship Prithvi III missile, which has been developed for the Indian Navy.  The  missile can be used as an anti-ship weapon as well as for destroying land targets depending on the range The missile gives the Indian Navy the capability to strike enemy targets with great precision.”);
+L2.setIcon(new ImageIcon(“E:\\ip\\weapons pic\\New folder\\DHA.JPG”));
